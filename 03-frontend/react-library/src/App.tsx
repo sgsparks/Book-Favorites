aeac70121baee395dbf4d7730b6f617637058de4
sam <sparks>
2023-12-18 13:04:50 -0800

    added layouts

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/NavBar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
-    <div className="App">
-     <h1>booya</h1>
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+      <Carousel/>
     </div>
+
+
   );
 }
 
