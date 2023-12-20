4cce488ee6fb78a7fb4b1288ed455421cc080013
sam <sparks>
2023-12-20 15:05:41 -0800

    spinner created

--- 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,7 +1,7 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
-// import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { Link } from "react-router-dom";
 
 export const Carousel = () => {
@@ -52,8 +52,7 @@ export const Carousel = () => {
 
     if (isLoading) {
         return (
-            <p>asdfa</p>
-            // <SpinnerLoading/>
+            <SpinnerLoading/>
         )
     }
 
