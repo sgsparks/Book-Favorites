6ea6e6d00fb89107270ea06af7bc36691d6a7172
sam <sparks>
2023-12-27 18:44:20 -0800

    changes

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/NavBar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/searchBooksPage/SearchBooksPage';
 export const App = ()=> {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage/>
       <Footer />
     </div>
   );
